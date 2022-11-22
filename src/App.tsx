4c966171f4eb83b31754b91c4624962ec3707526
Elena <eshaipuka@microsoft.com>
2022-11-22 20:57:39 +0200

    Fix background color

--- src/App.tsx
+++ src/App.tsx
@@ -41,21 +41,13 @@ const Dropdown = ({ data, onSelect }) => {
 }
 
 const App = () =>
-    <div className="dark h-full min-h-full">
-        <div className="h-full min-h-full bg-white dark:bg-gray-800 text-gray-900 dark:text-white">
+    <div className="dark h-full min-h-full min-h-screen">
+        <div className="h-full min-h-full min-h-screen bg-white dark:bg-gray-800 text-gray-900 dark:text-white">
             <SelectDifficultyDropdown/>
             <SelectFieldSizeDropdown/>
             <h1 className="font-medium leading-tight text-5xl mt-0 mb-2 text-center">Blockhead</h1>
             <br/>
             <Game/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
         </div>
     </div>
 

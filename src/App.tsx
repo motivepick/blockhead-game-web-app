412d3cad926a5c9276a65fb8189a6bd6fc30d781
Sergey Yaskov <yaskovdev@gmail.com>
2024-09-03 22:30:55 +0300

    Improve the buttons a bit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
 // @ts-nocheck
-import React, {useEffect} from 'react'
+import React, {useCallback, useEffect} from 'react'
 import {useAppDispatch} from './store/hooks'
 import {
     fetchComputerMove,
     fetchCreateNewField,
     fetchHint,
+    removeLetter,
     resetWord,
     setDifficulty,
     setFieldSize,
     userMove
 } from './store/reducer'
 import {useSelector} from 'react-redux'
-import {selectDifficulty, selectErrors, selectField, selectFieldSize} from './store/selectors'
+import {
+    selectDifficulty,
+    selectErrors,
+    selectField,
+    selectFieldSize,
+    selectHinting,
+    selectLastSetLetterId,
+    selectWordPath
+} from './store/selectors'
 import Board from './board/Board'
 import ScoreBoard from './board/ScoreBoard'
 import Background from './components/Background'
+import {ACTIVE_PRIMARY_BUTTON, ACTIVE_SECONDARY_BUTTON, DISABLED_BUTTON} from "./const";
 
 const SelectDifficultyDropdown = () => {
     const dispatch = useAppDispatch()
@@ -67,6 +77,20 @@ const App = () => {
 
     const field = useSelector(selectField)
     const errors = useSelector(selectErrors)
+    const wordPath = useSelector(selectWordPath)
+    const lastSetLetterId = useSelector(selectLastSetLetterId)
+    const hinting = useSelector(selectHinting)
+
+    const onResetWord = useCallback(() => {
+        if (wordPath.length) {
+            dispatch(resetWord());
+        } else if (lastSetLetterId) {
+            dispatch(removeLetter({cell: lastSetLetterId}))
+        }
+    }, [wordPath, dispatch, resetWord])
+
+    const canReset = !hinting && (wordPath.length || lastSetLetterId)
+
     if (field[0].length <= 0) return <div>Select field size</div>
 
     const onSubmitWord = async () => {
@@ -74,8 +98,6 @@ const App = () => {
         dispatch(userMove())
         dispatch(fetchComputerMove())
     }
-
-    const onResetWord = () => dispatch(resetWord())
 
     const onHint = () => dispatch(fetchHint())
 
@@ -95,23 +117,26 @@ const App = () => {
                     )}
                     <br/>
                     <button
-                        className="h-10 px-6 font-semibold rounded-md border border-slate-200 dark:border-slate-600 text-slate-900 dark:text-gray-200"
+                        className={hinting ? DISABLED_BUTTON : ACTIVE_PRIMARY_BUTTON}
                         type="button"
                         onClick={onSubmitWord}
+                        disabled={hinting}
                     >
                         Submit chosen word
                     </button>
                     <button
-                        className="h-10 px-6 font-semibold rounded-md border border-slate-200 dark:border-slate-600 text-slate-900 dark:text-gray-200"
+                        className={canReset ? ACTIVE_SECONDARY_BUTTON : DISABLED_BUTTON}
                         type="button"
                         onClick={onResetWord}
+                        disabled={!canReset}
                     >
-                        Reset chosen word
+                        {wordPath.length ? 'Reset chosen word' : 'Reset letter'}
                     </button>
                     <button
-                        className="h-10 px-6 font-semibold rounded-md border border-slate-200 dark:border-slate-600 text-slate-900 dark:text-gray-200"
+                        className={hinting ? DISABLED_BUTTON : ACTIVE_SECONDARY_BUTTON}
                         type="button"
                         onClick={onHint}
+                        disabled={hinting}
                     >
                         Hint
                     </button>

514cd4aaec0f89462af7fde2b649ab5cf4b61bc8
Sergey Yaskov <yaskovdev@gmail.com>
2025-11-05 02:22:10 +0200

    Switch to the Haskell back end.

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://blockhead-game-back-end.azurewebsites.net/api'
+    baseURL: 'https://blockhead-game-back-end-dqgbe9d4bbdcdfhz.northeurope-01.azurewebsites.net/api'
 })
 
 export default api

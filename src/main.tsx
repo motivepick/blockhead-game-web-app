4ea815d4815eab65a53cb1ca321b337567f30847
Sergey Yaskov <yaskovdev@gmail.com>
2025-11-11 00:26:27 +0200

    Fix i18n.

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
-import { Provider } from "react-redux"
-import { App } from "./App"
-import { store } from "./store/store"
+import {StrictMode} from "react"
+import {createRoot} from "react-dom/client"
+import {Provider} from "react-redux"
+import {App} from "./App"
+import {store} from "./store/store"
+import './i18n'
 import "./index.css"
 
 const container = document.getElementById("root")
